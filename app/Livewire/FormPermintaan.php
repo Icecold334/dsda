a5540c171424a8aeecc74efc58b5533cc453cae3
Icecold334 <fauzanimam334@gmail.com>
2024-11-19 11:48:54 +0700

    resume

--- app/Livewire/FormPermintaan.php
+++ app/Livewire/FormPermintaan.php
@@ -2,8 +2,9 @@
 
 namespace App\Livewire;
 
-use App\Models\UnitKerja;
+use Carbon\Carbon;
 use Livewire\Component;
+use App\Models\UnitKerja;
 
 class FormPermintaan extends Component
 {
@@ -21,12 +22,24 @@ class FormPermintaan extends Component
         }
         $this->dispatch('unit_id', unit_id: $this->unit_id);
     }
+    public function updatedTanggalPermintaan()
+    {
+        $this->dispatch('tanggal_permintaan', tanggal_permintaan: $this->tanggal_permintaan);
+    }
+    public function updatedKeterangan()
+    {
+        $this->dispatch('keterangan', keterangan: $this->keterangan);
+    }
+
     public function updatedSubUnitId()
     {
         $this->dispatch('sub_unit_id', sub_unit_id: $this->sub_unit_id);
     }
+
     public function mount()
     {
+
+        $this->tanggal_permintaan = Carbon::now()->format('Y-m-d');;
         $this->units = UnitKerja::whereNull('parent_id')->whereHas('children', function ($sub) {
             return $sub;
         })->get();
