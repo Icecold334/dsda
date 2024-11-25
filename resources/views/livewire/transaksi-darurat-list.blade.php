425ca79f1e10e0d2516c04c9ab2574d18f7a5aa0
Icecold334 <fauzanimam334@gmail.com>
2024-11-25 14:20:52 +0700

    pushh

--- resources/views/livewire/transaksi-darurat-list.blade.php
+++ resources/views/livewire/transaksi-darurat-list.blade.php
@@ -285,7 +285,7 @@
     </table>
 
     @if (true)
-        @if ($vendor_id && count($list) > 0 && $dokumenCount > 0 && $nomor_kontrak && $tanggal_kontrak)
+        @if ($vendor_id && count($list) > 0)
             {{-- @if (true) --}}
             <div class="flex justify-center"><button wire:click='saveKontrak'
                     class="text-primary-900 bg-primary-100 border border-primary-600 hover:bg-primary-600 hover:text-white  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 transition duration-200">Simpan</button>
