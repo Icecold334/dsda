ed16c278fb1095b90ab02e1ee8e233188f0cb9b7
Icecold334 <fauzanimam334@gmail.com>
2024-12-03 12:22:12 +0700

    some fix

--- resources/views/darurat/edit.blade.php
+++ resources/views/darurat/edit.blade.php
@@ -1,7 +1,7 @@
 <x-body>
     <div class="flex justify-between py-2 mb-3">
 
-        <h1 class="text-2xl font-bold text-primary-900 ">PERBARUI TRANSAKSI {{ $roles }}</h1>
+        <h1 class="text-2xl font-bold text-primary-900 ">PERBARUI TRANSAKSI </h1>
         <div>
             <a href="{{ route('transaksi-darurat-stok.index') }}"
                 class="text-primary-900 bg-primary-100 hover:bg-primary-600 hover:text-white  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 transition duration-200">Kembali</a>
