a1de5479f93b3ffd6d78cea8f5c5a8bfd3b266d0
RizafiDev <firmansyah.riza6@smk.belajar.id>
2025-07-29 13:44:29 +0700

    add: lampiran penyeseusaian barang

--- app/Models/FileSource.php
+++ app/Models/FileSource.php
@@ -7,13 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class FileSource extends Model
 {
     protected $guarded = [];
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    // Relasi polimorfis
+    // Relasi polimorfis for approvals
     public function approvable()
+    {
+        return $this->morphTo();
+    }
+
+    // Relasi polimorfis for file attachments
+    public function fileable()
     {
         return $this->morphTo();
     }
