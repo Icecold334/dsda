377f9d0762488d8562007dd7a31363c722b38fba
Icecoldl334 <fauzanimam334@gmail.com>
2025-07-31 15:57:51 +0700

    disable bug

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -37,7 +37,7 @@ abstract class Controller
                 // Periksa jika NIP atau TTD kosong
                 // dd(empty($user->nip) || empty($user->ttd));
                 if (empty($user->nip) || empty($user->ttd)) {
-                    dd('asd');
+                    // dd('asd');
                     session()->flash('alert');
                 }
             }
