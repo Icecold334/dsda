cdbd64615d1785e9c4db3c908871a388f339f3ad
Ugo <furqon.wkwk@gmail.com>
2025-10-31 10:36:03 +0700

    bug tambah barang

--- resources/views/livewire/searchable-select.blade.php
+++ resources/views/livewire/searchable-select.blade.php
@@ -1,16 +1,17 @@
-<div x-data="{ open: false }" class="relative w-full">
+<div x-data="{ open: @entangle('open') }" @click.away="open = false" class="relative w-full">
     <input type="text"
         class="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
-        placeholder="Cari..." wire:model.live="search" @focus="open = true" @click.away="open = false" />
+        placeholder="Cari..." wire:model.live.debounce.300ms="search" @focus="open = true" @click.away="open = false" />
 
     <ul x-show="open"
-        class="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-lg max-h-60 overflow-y-auto shadow-lg">
+        class="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-lg max-h-60 overflow-y-auto shadow-lg" style="display: none;">
         @forelse($filteredOptions as $item)
         @php
-        $view = is_array($item) ? $item[$label] : $item->{$label};
+            $view = is_array($item) ? ($item[$label] ?? '') : ($item->{$label} ?? '');
+            $itemId = is_array($item) ? ($item['id'] ?? null) : ($item->id ?? null);
         @endphp
         <li class="px-4 py-2 cursor-pointer hover:bg-blue-100"
-            wire:click="select({{ is_array($item) ? $item['id'] : $item->id }})" @click="open = false">
+            wire:click="select('{{ $itemId }}')">
             {{ $view }}
         </li>
         @empty
