92bd610449a07b846883ff01e63e23ba88d397dd
Icecold334 <fauzanimam334@gmail.com>
2026-07-06 11:14:14 +0700

    feat: add DatabaseSeeder and KasatpelBaruSeeder to generate mock inventory and request data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -72,11 +72,11 @@ class DatabaseSeeder extends Seeder
             RabSeeder::class,
             DetailPermintaanMaterialSeeder::class,
             KontrakSeeder::class,
-            // TransaksiSeeder::class,
+                // TransaksiSeeder::class,
             AkunSudinSeeder::class,
             AdminMaintenanceSeeder::class,
             DriverSecuritySeeder::class,
-            KasatpelBaruSeeder::class,
+            // KasatpelBaruSeeder::class,
         ]);
 
 
--- database/seeders/KasatpelBaruSeeder.php
+++ database/seeders/KasatpelBaruSeeder.php
@@ -16,117 +16,108 @@ class KasatpelBaruSeeder extends Seeder
     public function run(): void
     {
         Model::unguard();
+
+
+        // $user->roles()->syncWithoutDetaching([$role->id]);
+
+        // $this->command->info("User [{$user->email}] seeded successfully.");
+
+
         // Bersihkan duplikasi data seeder sebelumnya
-        User::where('email', 'kasatpel.selatan.setiabudi@test.com')->delete();
+        // User::where('email', 'kasatpel.selatan.setiabudi@test.com')->delete();
 
         $roleKasatpel = Role::firstOrCreate([
             'name' => 'Kepala Satuan Pelaksana',
             'guard_name' => 'web',
         ]);
 
-        $roleStaff = Role::firstOrCreate([
-            'name' => 'Pengurus Barang',
-            'guard_name' => 'web',
-        ]);
 
         // Unit Kerja Utama secara dinamis
         $unitUtara = UnitKerja::where('nama', 'Suku Dinas Sumber Daya Air Kota Administrasi Jakarta Utara')->first();
         $unitSelatan = UnitKerja::where('nama', 'Suku Dinas Sumber Daya Air Kota Administrasi Jakarta Selatan')->first();
 
-        // Sub-unit untuk Tatang Sunjaya (Seksi Sarana Jakarta Utara)
-        $seksiSaranaUtara = UnitKerja::where('nama', 'Seksi Pengelolaan Sarana Pengendali Banjir, Air Bersih dan Air Limbah')
-            ->where('parent_id', $unitUtara?->id)
-            ->first();
 
         // Data Users No. 1 sampai 6
-        $usersData = [
-            // 1. Tatang Sunjaya
+        $usersData =
             [
-                'match' => ['email' => 'tatang.sunjaya@test.com'],
-                'data' => [
-                    'name' => 'Tatang Sunjaya',
-                    'username' => 'tatang.sunjaya',
-                    'nip' => '197605152009041003',
-                    'unit_id' => $seksiSaranaUtara?->id,
-                    'password' => bcrypt('!nventory@Utara2025'),
-                    'email_verified_at' => now(),
-                ],
-                'role_id' => $roleStaff->id,
-            ],
-            // 2. Novita Kesnanda
-            [
-                'match' => ['email' => 'kasatpel.utara.pademangan@test.com'],
-                'data' => [
+                // [
+                //     'match' => ['email' => 'tatang'],
+                //     'data' => [
+                //         'name' => 'Tatang Sunjaya',
+                //         'username' => 'tatang.sunjaya',
+                //         'nip' => '197605152009041003',
+                //         'unit_id' => $seksiSaranaUtara?->id,
+                //         'password' => bcrypt('!nventory@Utara2025'),
+                //         'email_verified_at' => now(),
+                //     ],
+                //     'role_id' => $roleStaff->id,
+                // ],
+                [
+                    'match' => ['email' => 'kasatpel.utara.pademangan'],
                     'name' => 'Novita Kesnanda',
                     'username' => 'kasatpel.utara.pademangan',
                     'nip' => '198311122010012030',
                     'kecamatan_id' => 10,
                     'unit_id' => $unitUtara?->id,
                     'password' => bcrypt('!nventory@Utara2025'),
-                    'email_verified_at' => now(),
+                    'role_id' => $roleKasatpel->id,
                 ],
-                'role_id' => $roleKasatpel->id,
-            ],
-            // 3. Hastyanti Hidayat (Kasatpel Setiabudi)
-            [
-                'match' => ['email' => 'sda.setiabudijaksel@gmail.com'], // Menggunakan email Gmail aktif
-                'data' => [
+                [
+                    'match' => ['email' => 'kasatpel.selatan.setiabudi'],
                     'name' => 'Hastyanti Hidayat',
                     'username' => 'kasatpel.selatan.setiabudi',
                     'nip' => '198508072010012034',
                     'kecamatan_id' => 32,
                     'unit_id' => $unitSelatan?->id,
                     'password' => bcrypt('!nventory@Selatan2025'),
-                    'email_verified_at' => now(),
+                    'role_id' => $roleKasatpel->id,
                 ],
-                'role_id' => $roleKasatpel->id,
-            ],
-            // 4. Retno Wulandari (Kasatpel Pasar Minggu)
-            [
-                'match' => ['email' => 'kasatpel.selatan.pasarminggu@test.com'],
-                'data' => [
+                [
+                    'match' => ['email' => 'kasatpel.selatan.pasarminggu'],
                     'name' => 'Retno Wulandari',
                     'username' => 'kasatpel.selatan.pasarminggu',
                     'nip' => '197904072010012025',
                     'kecamatan_id' => 27,
                     'unit_id' => $unitSelatan?->id,
                     'password' => bcrypt('!nventory@Selatan2025'),
-                    'email_verified_at' => now(),
+                    'role_id' => $roleKasatpel->id,
                 ],
-                'role_id' => $roleKasatpel->id,
-            ],
-            // 5. Rosi Surya Indah (Kasatpel Tebet)
-            [
-                'match' => ['email' => 'kasatpel.selatan.tebet@test.com'],
-                'data' => [
+                [
+                    'match' => ['email' => 'kasatpel.selatan.tebet'],
                     'name' => 'Rosi Surya Indah',
                     'username' => 'kasatpel.selatan.tebet',
                     'nip' => '197404222014122001',
                     'kecamatan_id' => 31,
                     'unit_id' => $unitSelatan?->id,
                     'password' => bcrypt('!nventory@Selatan2025'),
-                    'email_verified_at' => now(),
+                    'role_id' => $roleKasatpel->id,
                 ],
-                'role_id' => $roleKasatpel->id,
-            ],
-            // 6. Zulfah Huzaifah (Kasatpel Pesanggrahan)
-            [
-                'match' => ['email' => 'kasatpel.selatan.pesanggrahan@test.com'],
-                'data' => [
+                [
+                    'match' => ['email' => 'kasatpel.selatan.pesanggrahan'],
                     'name' => 'Zulfah Huzaifah',
                     'username' => 'kasatpel.selatan.pesanggrahan',
                     'nip' => '198404272015042002',
                     'kecamatan_id' => 26,
                     'unit_id' => $unitSelatan?->id,
                     'password' => bcrypt('!nventory@Selatan2025'),
-                    'email_verified_at' => now(),
+                    'role_id' => $roleKasatpel->id,
                 ],
-                'role_id' => $roleKasatpel->id,
-            ],
-        ];
+            ];
 
         foreach ($usersData as $uData) {
-            $user = User::updateOrCreate($uData['match'], $uData['data']);
+            $count = User::where('email', $uData['match']['email'] . '@test.com')->count();
+            $user = User::firstOrCreate(
+                ['email' => $uData['match']['email'] . $count + 1 . '@test.com'],
+                [
+                    'name' => $uData['name'],
+                    'username' => $uData['username'] . $count + 1,
+                    'nip' => $uData['nip'],
+                    'kecamatan_id' => $uData['kecamatan_id'],
+                    'unit_id' => $uData['unit_id'],
+                    'password' => $uData['password'],
+                    'email_verified_at' => now(),
+                ]
+            );
             $user->roles()->syncWithoutDetaching([$uData['role_id']]);
             $this->command->info("User [{$user->email}] berhasil di-seed.");
         }
